95194840f1be7c145b9dde74f3f923846818ef20
Amir Mozafari <all1um@MacBook-Pro.local>
2023-10-04 17:56:57 -0700

    Fix port binding issues

--- index.js
+++ index.js
@@ -9,45 +9,36 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+
+
 
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('home', {
-    pageTitle: 'Home'
-  });  
+  res.render('home', { pageTitle: 'Home' });
 });
 
 app.get('/about', (req, res) => {
-  res.render('about', {
-    pageTitle: 'About Me'
-  });  
+  res.render('about', { pageTitle: 'About Me' });
 });
 
 app.get('/projects', (req, res) => {
-  res.render('projects', {
-    pageTitle: 'Projects'
-  }); 
+  res.render('projects', { pageTitle: 'Projects' });
 });
 
 app.get('/services', (req, res) => {
-  res.render('services', {
-    pageTitle: 'Services'
-  });  
+  res.render('services', { pageTitle: 'Services' });
 });
 
 app.get('/contact', (req, res) => {
-  res.render('contact', {
-    pageTitle: 'Contact Me'
-  }); 
+  res.render('contact', { pageTitle: 'Contact Me' });
 });
 
+
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
+}).on('error', err => {
+  console.log('Error:', err);
 });
