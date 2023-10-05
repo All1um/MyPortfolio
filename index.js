658f3513074f311e742cda12cb34a6c95d9a7a3d
Amir Mozafari <all1um@MacBook-Pro.local>
2023-10-04 17:38:02 -0700

    Fix port binding issue

--- index.js
+++ index.js
@@ -8,31 +8,44 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('home');
+  res.render('home', {
+    pageTitle: 'Home'
+  });  
 });
 
 app.get('/about', (req, res) => {
-  res.render('about');
+  res.render('about', {
+    pageTitle: 'About Me'
+  });  
 });
 
 app.get('/projects', (req, res) => {
-  res.render('projects');
+  res.render('projects', {
+    pageTitle: 'Projects'
+  }); 
 });
 
 app.get('/services', (req, res) => {
-  res.render('services');
+  res.render('services', {
+    pageTitle: 'Services'
+  });  
 });
 
 app.get('/contact', (req, res) => {
-  res.render('contact');
+  res.render('contact', {
+    pageTitle: 'Contact Me'
+  }); 
 });
 
 app.listen(port, () => {
